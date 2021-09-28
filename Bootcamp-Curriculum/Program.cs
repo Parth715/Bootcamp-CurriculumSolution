95362ae08d54bb98b32b5725f0d7b86eaf43d3f2
Parth Patel <parth2002715@gmail.com>
2021-09-28 13:11:53 -0400

    Add project files.

---
+++ Bootcamp-Curriculum/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Bootcamp_Curriculum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Git = new Topic();
+            Git.Name = "Git/Github";
+            Git.Category = "SCM";
+            Git.Difficulty = 2;
+            Git.Print();
+            var SQL = new Topic()
+            {
+                Name = "SQL Server",
+                Category = "Database",
+                Difficulty = 3,
+            };
+            SQL.Print();
+            var CSharp = new Topic();
+            CSharp.Name = "Git/Github";
+            CSharp.Category = "SCM";
+            CSharp.Difficulty = 2;
+            CSharp.Print();
+
+
+        }
+    }
+}
---
+++ Bootcamp-Curriculum/Topic.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bootcamp_Curriculum
+{
+    class Topic
+    {
+        private static int NextId { get; set; } = 5;
+        public int Id { get; private set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public int Difficulty { get; set; }
+
+        public void Print()
+        {
+            Console.WriteLine($"Id: {Id} | Name: {Name} | Category: {Category} | Difficulty: {Difficulty}");
+        }
+        public Topic()
+        {
+            this.Id = NextId++;
+        }
+    }
+}
